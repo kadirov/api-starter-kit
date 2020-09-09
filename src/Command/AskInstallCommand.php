913e453f4837338ee1e1f451b73125f36861b12e
Akmal Kadirov <akmal@kadirov.org>
2020-09-10 00:08:11 +0500

    add (AskInstallCommand)

---
+++ src/Command/AskInstallCommand.php
@@ -0,0 +1,78 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Command\Interfaces\GetOutputInterface;
+use App\Command\Traits\RunCommandTrait;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Throwable;
+
+class AskInstallCommand extends Command implements GetOutputInterface
+{
+    use RunCommandTrait;
+
+    protected static $defaultName = 'ask:install';
+
+    private OutputInterface $output;
+    private SymfonyStyle $symfonyIO;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        string $name = null
+    ) {
+        parent::__construct($name);
+        $this->entityManager = $entityManager;
+    }
+
+    public function getOutput(): OutputInterface
+    {
+        return $this->output;
+    }
+
+    public function getSymfonyStyleOutput(): SymfonyStyle
+    {
+        return $this->symfonyIO;
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription("Do first settings for Api-Starter-Kit");
+    }
+
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ): int {
+        $this->symfonyIO = new SymfonyStyle($input, $output);
+        $this->output = $output;
+
+        $this->waitConnection();
+        $this->runCommandAndNotify('ask:deploy');
+        $this->runCommandAndNotify('ask:generate:jwtKeys');
+
+        $this->symfonyIO->success('Successfully installed');
+
+        return 0;
+    }
+
+    private function waitConnection(): void
+    {
+        while (true) {
+            try {
+                $this->entityManager->getConnection()->connect();
+                if ($this->entityManager->getConnection()->isConnected()) {
+                    return;
+                }
+            } catch (Throwable $exception) {
+                $this->symfonyIO->writeln("MySQL server isn't up yet. Let's wait until it ups...");
+                sleep(10);
+            }
+        }
+    }
+}
