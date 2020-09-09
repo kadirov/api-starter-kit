38d23d9c754941cc1540c80125134ec6fbea47c9
Akmal Kadirov <akmal@kadirov.org>
2020-09-09 16:34:48 +0500

    edit (Controller) change user by $id instead of current user

--- src/Controller/UserChangePasswordAction.php
+++ src/Controller/UserChangePasswordAction.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Component\User\Dtos\UserPasswordDto;
 use App\Component\User\UserManager;
 use App\Controller\Base\AbstractController;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,15 +19,20 @@ class UserChangePasswordAction extends AbstractController
 {
     public function __invoke(
         Request $request,
-        UserManager $userManager
+        UserManager $userManager,
+        UserRepository $repository,
+        int $id
     ): Response {
+        /** @var User $user */
+        $user = $this->getEntityOrError($repository, $id);
+
         /** @var UserPasswordDto $userDto */
         $userDto = $this->getDtoFromRequest($request, UserPasswordDto::class);
         $this->validate($userDto);
 
-        $userManager->hashPassword($this->getUser(), $userDto->getPassword());
-        $userManager->save($this->getUser(), true);
+        $userManager->hashPassword($user, $userDto->getPassword());
+        $userManager->save($user, true);
 
-        return $this->responseNormalized($this->getUser());
+        return $this->responseNormalized($user);
     }
 }
--- src/Controller/UserDeleteAction.php
+++ src/Controller/UserDeleteAction.php
@@ -5,15 +5,22 @@ namespace App\Controller;
 use App\Component\Core\MarkEntityAsDeleted;
 use App\Component\User\UserManager;
 use App\Controller\Base\AbstractController;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserDeleteAction extends AbstractController
 {
     public function __invoke(
         UserManager $manager,
-        MarkEntityAsDeleted $markEntityAsDeleted
+        MarkEntityAsDeleted $markEntityAsDeleted,
+        UserRepository $repository,
+        int $id
     ): Response {
-        $markEntityAsDeleted->mark($this->getUser(), $manager);
+        /** @var User $user */
+        $user = $this->getEntityOrError($repository, $id);
+
+        $markEntityAsDeleted->mark($user, $manager);
         return $this->responseEmpty();
     }
 }
