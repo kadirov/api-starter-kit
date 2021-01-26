e0d806f2c6803e7444284862bca44945049f2952
Akmal Kadirov <akmal@kadirov.org>
2021-01-26 17:46:15 +0500

    add (docker) supervison conf

--- docker/php/run-deamons.sh
+++ docker/php/run-deamons.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cron -f &
+# uncomment if you use workers
+#service supervisor start
+#supervisorctl reread
+#supervisorctl update
+#supervisorctl start messenger-consume:*
 docker-php-entrypoint php-fpm
