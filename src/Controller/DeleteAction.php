9f868716407672706eeaec7bbb494cc5aebcb709
Akmal Kadirov <akmal@kadirov.org>
2021-01-26 16:11:45 +0500

    add (DeleteAction) return type

--- src/Controller/DeleteAction.php
+++ src/Controller/DeleteAction.php
@@ -1,14 +1,16 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 namespace App\Controller;
 
 use App\Component\Core\MarkEntityAsDeleted;
 use App\Controller\Base\AbstractController;
 use App\Entity\Interfaces\IsDeletedSettableInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class DeleteAction extends AbstractController
 {
-    public function __invoke(IsDeletedSettableInterface $data, MarkEntityAsDeleted $markEntityAsDeleted)
+    public function __invoke(IsDeletedSettableInterface $data, MarkEntityAsDeleted $markEntityAsDeleted): Response
     {
         $markEntityAsDeleted->mark($data);
         return $this->responseEmpty();
