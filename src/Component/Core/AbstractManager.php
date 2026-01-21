a5019cb3eb4b5cf0fc7dbfe06ead3d535528f3b8
Akmal Kadirov <akmal@kadirov.org>
2026-01-21 17:32:01 +0500

    edit (AbstractManager)

--- src/Component/Core/AbstractManager.php
+++ src/Component/Core/AbstractManager.php
@@ -4,23 +4,28 @@ declare(strict_types=1);
 
 namespace App\Component\Core;
 
+use App\Component\User\CurrentUser;
 use App\Entity\Interfaces\CreatedAtSettableInterface;
+use App\Entity\Interfaces\CreatedBySettableInterface;
 use App\Entity\Interfaces\UpdatedAtSettableInterface;
+use App\Entity\Interfaces\UpdatedBySettableInterface;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 
 abstract class AbstractManager
 {
     private EntityManagerInterface $entityManager;
+    private CurrentUser $currentUser;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, CurrentUser $currentUser)
     {
         $this->entityManager = $entityManager;
+        $this->currentUser = $currentUser;
     }
 
     public function save(object $entity, bool $needToFlush = false): void
     {
-        $this->updateCreatedOrUpdatedDates($entity);
+        $this->updateAuditFields($entity);
         $this->getEntityManager()->persist($entity);
 
         if ($needToFlush) {
@@ -33,12 +38,36 @@ abstract class AbstractManager
         return $this->entityManager;
     }
 
-    private function updateCreatedOrUpdatedDates(object $entity): void
+    private function updateAuditFields(object $entity): void
     {
-        if ($entity->getId() === null && $entity instanceof CreatedAtSettableInterface) {
+        $isNew = $entity->getId() === null;
+
+        if ($isNew) {
+            $this->setCreatedFields($entity);
+        } else {
+            $this->setUpdatedFields($entity);
+        }
+    }
+
+    private function setCreatedFields(object $entity): void
+    {
+        if ($entity instanceof CreatedAtSettableInterface) {
             $entity->setCreatedAt(new DateTime());
-        } elseif ($entity->getId() !== null && $entity instanceof UpdatedAtSettableInterface) {
+        }
+
+        if ($entity instanceof CreatedBySettableInterface && $this->currentUser->isAuthed()) {
+            $entity->setCreatedBy($this->currentUser->getUser());
+        }
+    }
+
+    private function setUpdatedFields(object $entity): void
+    {
+        if ($entity instanceof UpdatedAtSettableInterface) {
             $entity->setUpdatedAt(new DateTime());
+        }
+
+        if ($entity instanceof UpdatedBySettableInterface && $this->currentUser->isAuthed()) {
+            $entity->setUpdatedBy($this->currentUser->getUser());
         }
     }
 }
